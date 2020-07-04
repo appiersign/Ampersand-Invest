ad52ddfeab829438dec3fd2209edc64e8581f0c0
Solomon APPIER-SIGN <braasig@gmail.com>
2020-07-04 19:43:03 +0000

    registered ProjectListComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {ProjectListComponent} from './project-list/project-list.component';
 
 const routes = [
-  {
-    path: '',
-    component: LoginComponent
-  }
+  {path: '', component: LoginComponent},
+  {path: 'projects', component: ProjectListComponent}
 ];
 
 @NgModule({
@@ -15,4 +14,4 @@ const routes = [
 })
 
 export class AppRoutingModule {}
-export const routingComponents = [LoginComponent];
+export const routingComponents = [LoginComponent, ProjectListComponent];
