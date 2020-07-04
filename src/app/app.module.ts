373e4e2a9958e8958bd8df7b1cdc828e434c9474
Solomon APPIER-SIGN <braasig@gmail.com>
2020-07-04 19:40:11 +0000

    generated project-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    ProjectListComponent
   ],
   imports: [
     BrowserModule,
