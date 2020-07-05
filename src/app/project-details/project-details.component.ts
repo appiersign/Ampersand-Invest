9cd4eb45864e4539efb16d4703987b90ba900d50
Solomon APPIER-SIGN <braasig@gmail.com>
2020-07-05 18:21:06 +0000

    added Project Service added getStatus() and statusClass() to handle project approval status

--- src/app/project-details/project-details.component.ts
+++ src/app/project-details/project-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ProjectService} from '../project.service';
 
 @Component({
   selector: 'app-project-details',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-details.component.css']
 })
 export class ProjectDetailsComponent implements OnInit {
+  public project = {is_approved: false, company: {}};
 
-  constructor() { }
+  constructor(private _projectService: ProjectService, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this._projectService.getProjects()
+      .subscribe(data => this.project = data.find(d => d.id === this.route.snapshot.paramMap.get('id')));
+  }
+
+  getStatus() {
+    return (this.project.is_approved) ? 'approved' : 'pending';
+  }
+
+  statusClass(status: boolean) {
+    return {'badge-success': status, 'badge-warning': !status};
   }
 
 }
