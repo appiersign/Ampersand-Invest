9a6e65662f4a2c870396ba9d754d3d86e320d311
Solomon APPIER-SIGN <braasig@gmail.com>
2020-07-05 18:15:35 +0000

    added extra Project fields

--- src/app/project.ts
+++ src/app/project.ts
@@ -2,4 +2,10 @@ export interface Project {
   id: string;
   name: string;
   summary: string;
+  market: string;
+  market_strategy: string;
+  is_approved: boolean;
+  company: {
+    category: string;
+  };
 }
