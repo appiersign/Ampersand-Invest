8e0a50978f1db1d54bd7f5ff7f81cbbb0cd8b875
Solomon APPIER-SIGN <braasig@gmail.com>
2020-07-06 14:50:22 +0000

    added auth service change static token to auth user token

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -2,11 +2,12 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Project} from './project';
 import {Observable} from 'rxjs/Observable';
+import {AuthService} from './auth.service';
 
 @Injectable()
 export class ProjectService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private authService: AuthService) {
   }
 
   getProjects(): Observable<Project[]> {
@@ -14,8 +15,7 @@ export class ProjectService {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        // tslint:disable-next-line:max-line-length
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODhcL2FwaVwvYXV0aFwvbG9naW4iLCJpYXQiOjE1OTM3NzExMzcsImV4cCI6MTU5MzgxNDMzNywibmJmIjoxNTkzNzcxMTM3LCJqdGkiOiJRbnhITVZrQUJIM1dBaUtQIiwic3ViIjoxLCJwcnYiOiJjZjI4NGMyYjFlMDZmMzNjMjZiZDU3OTc1NjZkOWZkNzRiZTExYmY1In0.AO5V1DDqUJes9wZuSoj1xiIoFEx_qAOz-jZGlMH4gyk'
+        'Authorization': `Bearer ${this.authService.getToken()}`
       }
     });
   }
