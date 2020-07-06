43f53a61459eeb3a33483f4c439f680169079bc9
Solomon APPIER-SIGN <braasig@gmail.com>
2020-07-06 14:45:35 +0000

    added auth guard to check for auth user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {ProjectListComponent} from './project-list/project-list.component';
 import {ProjectDetailsComponent} from './project-details/project-details.component';
+import {AuthGuard} from './auth.guard';
 
 const routes = [
   {path: '', component: LoginComponent},
-  {path: 'projects', component: ProjectListComponent},
-  {path: 'projects/:id', component: ProjectDetailsComponent}
+  {path: 'projects', component: ProjectListComponent, canActivate: [AuthGuard]},
+  {path: 'projects/:id', component: ProjectDetailsComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
