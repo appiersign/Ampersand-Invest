d73225fbd7a4330de3b866051b33954a05187bd6
Solomon APPIER-SIGN <braasig@gmail.com>
2020-07-06 14:51:40 +0000

    added auth guard

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, Router} from '@angular/router';
+import {AuthService} from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  canActivate(): boolean {
+    if (this.authService.loggedIn()) {
+      return true;
+    }
+
+    this.router.navigate(['']).then(r => r);
+    return false;
+  }
+}
